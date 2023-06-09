b613d7a11d298388acfc4c401a31178543d81517
venutti <lvenutti@fi.uba.ar>
2023-06-09 12:37:17 -0300

    Create handler functions to importAccount and createAccount in LogInWallet.svelte

--- src/routes/LogInWallet/LogInWallet.svelte
+++ src/routes/LogInWallet/LogInWallet.svelte
@@ -1,15 +1,48 @@
+<script>
+  import { useNavigate } from "svelte-navigator";
+  import { importKeypairFromSecret, createAccount } from "../../lib/stellar/stellar"
+  import { keypair } from "../../lib/store/global"
+  import { notifySuccess, notifyError } from "../../lib/utils/notification"; 
+
+  const navigate = useNavigate();
+
   let secretKey = "";
+
+  function handleImportAccount() {
+    try {
+      const importedKeypair = importKeypairFromSecret(secretKey);
+      keypair.set(importedKeypair);
+      navigate("/account");
+      notifySuccess("Account imported successfully");
+    } catch (error) {
+      notifyError("Invalid secret key: " + error.message);
+      console.error(error);
+    }
+  }
+
+  async function handleCreateAccount() {
+    try {
+      const createdKeypair = await createAccount()
+      keypair.set(createdKeypair);
+      navigate("/account");
+      notifySuccess("Account created successfully");
+    } catch (error) {
+      console.error(error);
+      notifyError("Failed account creation: " + error.message);
+    }
+  }
+</script>
+
 <div class="login-wallet">
   <h1>Welcome to you Stellar Wallet</h1>
 
-  <button>Create account</button>
+  <button on:click={handleCreateAccount}>Create account</button>
 
   <label>
     <span>If you have an account, import your secret key:</span>
     <div>
-      <input type="text" placeholder="Secret key" />
-      <button>Import</button>
       <input bind:value={secretKey} type="text" placeholder="Secret key" />
+      <button on:click={handleImportAccount}>Import</button>
     </div>
   </label>
 </div>
